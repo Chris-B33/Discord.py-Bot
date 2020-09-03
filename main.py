2efacb5be06e364c9e653f0e65371620dc806bbd
Chris <csbroman10@gmail.com>
2020-09-03 23:45:39 +0100

    Bot Initialisation

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-TOKEN = open("token.txt").read()
+from discord.ext import commands
+
+TOKEN = open("token.txt", "r").readline()
+
+client = commands.Bot(command_prefix="a!")
+
+@client.event
+async def on_ready():
+    print(f"{client.user} connected...")
+
+client.run(TOKEN)
