ec31247e710126334714345e0e27d0715a03eaf4
Chris <csbroman10@gmail.com>
2020-09-04 15:35:03 +0100

    Updated on_member_join and added settings.json

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import discord
+import discord, json
 from discord.ext import commands
 
 
-TOKEN           = open("token.txt", "r").readline()
-DEFAULT_ROLE    = "Newcomers"
-DEFAULT_PERMS   = discord.Permissions(permissions=0)
-DEFAULT_MSG     = "Welcome to the server! Hope you enjoy your stay :D"
+with open("settings.json", "r") as data:
+    settings = json.load(data)
 
 client = discord.ext.commands.Bot(command_prefix="a!")
 
@@ -16,18 +14,24 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
-    await member.send(DEFAULT_MSG)
+    await member.send(settings["welcome"])
 
     # Checks if default role is present
     for role in member.guild.roles:
-        if role.name == DEFAULT_ROLE:
-            role = discord.utils.get(member.server.roles, name=DEFAULT_ROLE)
+        name = settings["role"]["name"]
+        if role.name == name:
+            role = discord.utils.get(member.guild.roles, name=name)
             await member.add_roles(role)
             break
     else:
-        role = await member.guild.create_role(name=DEFAULT_ROLE,
-                                              colour=discord.Colour(0xf8f8f8),
-                                              permissions=DEFAULT_PERMS)
+        role = await member.guild.create_role(
+            name        = settings["role"]["name"],
+            colour      = discord.Colour(int(settings["role"]["colour"], base=16)),
+            permissions = discord.Permissions(settings["role"]["perms"]),
+            hoist       = settings["role"]["hoist"],
+            mentionable = settings["role"]["mentionable"],
+            reason      = settings["role"]["reason"]
+        )
         for member in member.guild.members:
             await member.add_roles(role)
 
@@ -37,4 +41,4 @@ async def on_guild_remove(guild):
     await owner.send("Alright then, I see how it is ;(")
 
 
-client.run(TOKEN)
+client.run(settings["token"])
